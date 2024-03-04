ef8cd8d587186e1533a2bfc721bc990e67b6690f
truongnqyh <87426062+truongnqyh@users.noreply.github.com>
2024-03-04 20:00:32 +0700

    Update main.c

--- NRF24L01/nRF24L01_Gateway/Core/Src/main.c
+++ NRF24L01/nRF24L01_Gateway/Core/Src/main.c
@@ -96,17 +96,17 @@ void set_pin_output(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 {
 	GPIO_InitTypeDef GPIO_InitStruct = {0};
 	GPIO_InitStruct.Pin = GPIO_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+  	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
 }
 void set_pin_input(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 {
 	GPIO_InitTypeDef GPIO_InitStruct = {0};
 	GPIO_InitStruct.Pin = GPIO_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+  	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  	GPIO_InitStruct.Pull = GPIO_NOPULL;
+  	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
 }
 void dht11_init(void)
 {
